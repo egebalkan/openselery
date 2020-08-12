d736be0db346dbbf30e0b8c9009983bc3fbc3c9e
johannes karoff <johannes@karoff.net>
2020-08-12 20:50:53 +0200

    wip

--- openselery/visualization.py
+++ openselery/visualization.py
@@ -2,7 +2,8 @@ import os
 import tempfile
 os.environ['MPLCONFIGDIR'] = tempfile.mkdtemp()
 import matplotlib.pyplot as plt
-from matplotlib.ticker import ScalarFormatter 
+from matplotlib.ticker import ScalarFormatter
+from itertools import accumulate
 import numpy as np
 import json
 import datetime
@@ -28,8 +29,11 @@ def transactionIsLastMonth(transaction):
     diff_date = now_date - creation_date
     return diff_date.total_seconds() <= 30 * 24 * 60 * 60
 
+def transactionIsEur(transaction):
+    return transaction["native_amount"]["currency"] == "EUR"
+
 def transactionIsEurSpent(transaction):
-    return float(transaction["native_amount"]["amount"]) < 0 and transaction["native_amount"]["currency"] == "EUR"
+    return float(transaction["native_amount"]["amount"]) < 0 and transactionToEur(transaction)
 
 def transactionToEur(transaction):
     assert transaction["native_amount"]["currency"] == "EUR"
@@ -54,16 +58,21 @@ def visualizeTransactions(resultDir, transactionFilePath):
         transactions = json.loads(transactions_file)
 
         # prepare transaction data
-        data_by_day_last_month = groupBy(filter(lambda t: transactionIsEurSpent(t) and transactionIsLastMonth(t), transactions["data"]), transactionToYearMonthDay)
-        data_by_year_month = groupBy(filter(transactionIsEurSpent, transactions["data"]), transactionToYearMonth)
-        data_by_user = groupBy(filter(transactionIsEurSpent, transactions["data"]), transactionToUserEmail)
+        data_by_day = groupBy(transactionToEur, transactions["data"]), transactionToYearMonthDay)
+        spent_data_by_day_last_month = groupBy(filter(lambda t: transactionIsEurSpent(t) and transactionIsLastMonth(t), transactions["data"]), transactionToYearMonthDay)
+        spent_data_by_year_month = groupBy(filter(transactionIsEurSpent, transactions["data"]), transactionToYearMonth)
+        spent_data_by_user = groupBy(filter(transactionIsEurSpent, transactions["data"]), transactionToUserEmail)
 
-        eur_by_day_last_month = { k: -1 * sum(map(transactionToEur, v)) for k,v in data_by_day_last_month.items() }
-        eur_by_year_month = { k: -1 * sum(map(transactionToEur, v)) for k,v in data_by_year_month.items() }
+        wallet_balance_btc_per_day = { k: -1 * accumulate(map(transactionToEur, v), initial = 0) for k,v in spent_data_by_day_last_month.items() }
+        eur_by_day_last_month = { k: -1 * sum(map(transactionToEur, v)) for k,v in spent_data_by_day_last_month.items() }
+        eur_by_year_month = { k: -1 * sum(map(transactionToEur, v)) for k,v in spent_data_by_year_month.items() }
         eur_by_user = { k: -1 * sum(map(transactionToEur, v)) for k,v in data_by_user.items() }
 
         # draw diagrams
         plt.rcdefaults()
+
+        drawBarChart("BTC wallet per day in last month", "BTC", wallet_balance_btc_by_day.keys(), wallet_balance_btc_by_day.values())
+        plt.savefig(resultDir + "/wallet_balance_per_day.png", bbox_inches = "tight" )
 
         drawBarChart("EUR transactions per day in last month", "EUR", eur_by_day_last_month.keys(), eur_by_day_last_month.values())
         plt.savefig(resultDir + "/transactions_per_day.png", bbox_inches = "tight" )
